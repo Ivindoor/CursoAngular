bcc5f521d0d20f46aa783540092d3aa2606c3425
J Alejandro Garcia <jalejandro.garcia@sanmina.com>
2019-12-20 22:18:13 +0100

    First version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-tour-of-heroe';
+  title = 'Tour of Heroes';
+  subtitle = 'Course';
 }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+
+    { id: 1, name: 'Scorpion', url: 'Scorpion.gif' },
+    { id: 2, name: 'Jacke', url: 'Jacke.gif' },
+    { id: 3, name: 'Link', url: 'Link.gif' },
+    { id: 4, name: 'Lumpy princess', url: 'Lumpy.gif' },
+    { id: 5, name: 'Snorlax', url: 'Snorlax.gif' },
+    { id: 6, name: 'Squid', url: 'Squid.gif' },
+    { id: 7, name: 'Stich', url: 'Stich.gif' },
+    { id: 8, name: 'Pikachu', url: 'Pikachu.gif' },
+    { id: 8, name: 'Ryo', url: 'Ryo.gif' }
+];
+
